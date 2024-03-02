423deccdce3b71fd5794559a3e6ac8a82ea2ff93
luis50019 <teddiazdiaz019@gmail.com>
2024-03-02 03:18:49 -0600

    configurando variables de entorno

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-const URL_BASE = 'http://localhost:4000'
+
+const URL =import.meta.env.VITE_URL_BASE || "http://localhost:4000";
 const instance = axios.create({
-	baseURL: URL_BASE,
+	baseURL: `${URL}/`,
 	withCredentials: true
 })
 
